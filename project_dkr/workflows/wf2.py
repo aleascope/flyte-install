43c7c227ff189bd5632b4b1805c04906732ec077
m.zaradzki <mathieu.zaradzki@gmail.com>
2025-02-24 15:41:54 +0100

    Update wf2.py

--- project_dkr/workflows/wf2.py
+++ project_dkr/workflows/wf2.py
@@ -1,18 +1,23 @@
 from flytekit import task, workflow
 import pandas as pd
-
+from typing import List
 
 @task
-def add(a: int, b: int) -> int:
+def tabulate(a: int, b: int) -> List[int]:
     nb_rows = 10
     df = pd.DataFrame({
         'a': [a] * nb_rows,
         'b': [b] * nb_rows
     })
     df['c'] = df['a'] + df['b']
-    return int(df['c'].sum())
+    return list(df['c'])
 
+@task
+def sum_table(l: List[int]) -> int:
+    return int(sum(l))
 
 @workflow
 def my_workflow(a: int = 3, b: int = 4) -> int:
-    return add(a=a, b=b)
+    df = tabulate(a=a, b=b)
+    s = sum_table(df)
+    return s
